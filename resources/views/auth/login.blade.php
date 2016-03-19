45d12c1d3d6ce343a9b581f386832975201911a8
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-03-19 06:46:57 -0300

    login and register ok

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 
 @section('content')
 <div class="uk-vertical-align uk-text-center uk-height-1-1 login-section">
-   <div class="uk-vertical-align-middle" style="width: 250px;">
+   <div class="uk-vertical-align-middle" style="width: 300px;">
       <img class="uk-margin-bottom" width="280" height="120" src="img/logo.png" alt="{{ trans('project.project-name') }}">
       <form class="uk-panel uk-panel-box uk-form" method="POST" action="{{ url('/login') }}">
          {!! csrf_field() !!}
@@ -27,10 +27,10 @@
          </div>
          <div class="uk-form-row">
             <button type="submit" class="uk-width-1-3 uk-button uk-button-danger uk-button-large">{{ trans('project.submit') }}</button>
-            <a class="uk-width-2-4 uk-button uk-button-primary uk-button-large" href="{{ URL('/login/facebook') }}">Facebook</a>
+            <a class="uk-width-2-4 uk-button uk-button-primary uk-button-large" href="{{ URL('/login/facebook') }}"><i class="uk-icon-facebook"></i> Facebook</a>
          </div>
          <div class="uk-form-row">
-            <a href="{{ URL('/register') }}" class="uk-button uk-width-1-1 uk-button-success uk-button-large">{{ trans('project.register')}}</a>
+            <a href="{{ URL('/register') }}" class="uk-button uk-width-1-1 uk-button-success uk-button-large"><i class="uk-icon-user-plus"></i> {{ trans('project.register')}}</a>
          </div>
       </form>
    </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,82 +1,56 @@
-@extends('layouts.app')
+@extends('project.general')
+@section('title')
+{{ trans('project.login') }} | {{ trans('project.project-name') }}
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                        {!! csrf_field() !!}
+<div class="uk-vertical-align uk-text-center uk-height-1-1 login-section">
+   <div class="uk-vertical-align-middle" style="width: 550px;">
+      <img class="uk-margin-bottom" width="280" height="120" src="img/logo.png" alt="{{ trans('project.project-name') }}">
+      <form class="uk-panel uk-panel-box uk-form" method="POST" action="{{ url('/login') }}">
+         {!! csrf_field() !!}
+         <div class="uk-form-row">
+            <input class="uk-width-1-1 uk-form-large" type="text" name="name" value="{{ old('name') }}" placeholder="Name" required>
+            @if ($errors->has('name'))
+                <span class="uk-alert uk-alert-danger">
+                    <strong>{{ $errors->first('name') }}</strong>
+                </span>
+            @endif
+         </div>
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Name</label>
+         <div class="uk-form-row">
+            <input class="uk-width-1-1 uk-form-large" type="email" name="email" value="{{ old('email') }}" placeholder="Email (exemplo@exemplo.com)" required>
+            @if ($errors->has('email'))
+                <span class="uk-alert uk-alert-danger">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
+         </div>
+         
+         <div class="uk-form-row">
+            <input class="uk-width-1-1 uk-form-large" type="password" name="password" value="{{ old('password') }}" placeholder="{{ trans('project.password') }}" required>
+            @if ($errors->has('password'))
+                <span class="uk-alert uk-alert-danger">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+            @endif
+         </div>
 
-                            <div class="col-md-6">
-                                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+         <div class="uk-form-row">
+            <input class="uk-width-1-1 uk-form-large" type="password" name="password_confirmation" value="{{ old('password_confirmation') }}" placeholder="Repetir senha" required>
+            @if ($errors->has('password_confirmation'))
+                <span class="uk-alert uk-alert-danger">
+                    <strong>{{ $errors->first('password_confirmation') }}</strong>
+                </span>
+            @endif
+         </div>
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+         <div class="uk-form-row">
+            <button type="submit" class="uk-width-1-3 uk-button uk-button-danger uk-button-large"><i class="uk-icon-user-plus"></i> Cadastrar</button>
+            <a class="uk-width-2-4 uk-button uk-button-primary uk-button-large" href="{{ URL('/login/facebook') }}"><i class="uk-icon-facebook"></i> Cadastrar com facebook</a>
+         </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input type="password" class="form-control" name="password_confirmation">
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-user"></i>Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+      </form>
+   </div>
 </div>
-@endsection
+@stop
