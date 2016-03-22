544e67e51e1b5fe69fdc8838d3b283204dec5b5a
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-03-22 14:34:37 -0300

    Events model

---
+++ database/migrations/2016_03_22_084323_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 255);
+            $table->text('descricao');
+            $table->datetime('start_date');
+            $table->datetime('end_date');
+            $table->boolean('real_or_game'); // evento astronômico real ou fake nosso
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
