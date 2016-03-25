7a79266ed86d97b058b06f6031fa4fbadc6ee781
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-03-25 16:53:58 -0300

    work on User progress

---
+++ app/UserProgres.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserProgres extends Model
+{
+    public $keys = 
+    [
+    	'tutorial', 'chapter1_button', 'chapter1_big_bang', 'chapter2_',
+    	
+    ];
+}
---
+++ database/migrations/2016_03_24_081119_create_user_progres_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserProgresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_progres', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('key');
+            
+            $table->string('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->boolean('completed')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_progres');
+    }
+}
