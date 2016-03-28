1d055b52448372263c7989961016b26119d7b35e
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-03-28 17:53:24 -0300

    game controller update

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use User;
+use App\User;
 use App\UserConfig;
 use App\UsersQuest;
 use App\UserBag;
 use App\Item;
 use App\Quest;
 use App\UserProgres;
+use App\UserInsignas;
 
 // toda a magia vai acontecer aqui :)
 class GameController extends Controller
@@ -21,7 +22,15 @@ class GameController extends Controller
 
     public function __construct() {
         $this->player_bar();
-        $this->view_vars = array_pop($this->view_vars);
+    }
+
+    private function view_vars(){
+        foreach($this->view_vars as $array_level1){
+            foreach($array_level1 as $key => $value){
+                $final[$key] = $value; 
+            }
+        }
+        return $final;
     }
 
     /**
@@ -29,7 +38,7 @@ class GameController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index() {       
+    public function index() {     
         $current_chapter = new UserProgres();
         $name = $current_chapter->current()->name;
 
@@ -41,39 +50,51 @@ class GameController extends Controller
         $chapter->key = 'welcome';
         $complete = $chapter->complete();
 
-        return view('game.chapters.tutorial', $this->view_vars);
+        return view('game.chapters.tutorial', $this->view_vars());
     }
 
     public function welcome() {
-        return view('game.chapters.welcome', $this->view_vars);
+        return view('game.chapters.welcome', $this->view_vars());
     }
 
     public function observatory() {
-        return view('game.general.observatory', $this->view_vars);
+        return view('game.general.observatory', $this->view_vars());
+    }
+
+    public function player(Request $request){
+        $user_id = $request->id;
+        $check_user = User::where('id', $user_id)->limit(1)->get()->first();
+        if(!$check_user){
+            return "Esse player não existe";
+        }
+        $this->view_vars[] = ['player' => $check_user];
+        $this->view_vars[] = ['player_patente' => User::patente($check_user->level)];
+
+        return view('game.general.player-public', $this->view_vars());
     }
 
     public function player_bar() {
-        \App\User::gain_xp(300);
         $this->view_vars[] = [
             'music_volume' => UserConfig::getConfig('music_volume'),
-            'xp_bar' => \App\User::xp_bar(),
+            'xp_bar' => User::xp_bar(),
             'user_name' => auth()->user()->name,
             'user_level' => auth()->user()->level,
             'user_money' => auth()->user()->money,
             'user_xp' => auth()->user()->xp,
-            'xp_for_next_level' => \App\User::xp_for_next_level(),
+            'xp_for_next_level' => User::xp_for_next_level(),
             'lang' => session()->get('language', 'pt-br'),
             'shop' => Item::shop(),
             'bag' => UserBag::bag(),
             'avaliable_quests' => Quest::avaliable_quests(),
             'accepted_quests' => Quest::accepted_quests(),
-            'patente' => \App\User::patente(),
+            'patente' => User::patente(),
+            'user_insignas' => \App\Insignas::all(),
         ];
     }
 
     // chapters
     public function chapter1() { 
-        return view('game.chapters.universe', $this->view_vars);
+        return view('game.chapters.universe', $this->view_vars());
 
     } // the universe
     public function chapter2() { } // galaxy clusters
