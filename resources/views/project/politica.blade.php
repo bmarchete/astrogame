38a034872542aa5fc7965165c8313e3b33ab47ee
Eduardo Ramos <eduardo.auramos@gmail.com>
2016-04-12 21:04:18 -0300

    política de privicidade e termos

--- resources/views/project/politica.blade.php
+++ resources/views/project/politica.blade.php
@@ -11,10 +11,7 @@
 		<div class="uk-witdh-1-1 uk-width-large-3-4 uk-align-center">
 <h1>Política de Privacidade</h1>
 	<div class="uk-text-justify">
-      <p>Todas as informações pessoais recolhidas, serão usadas para ajudar a tornar a sua visita no nosso site o mais produtiva e agradável possível, a garantia da confidencialidade dos dados pessoais dos utilizadores do nosso site é importante para a ETEC Social.</p>
-      <p>Todas as informações pessoais relativas a membros, assinantes, clientes ou visitantes que usem a ETEC Social serão tratadas em concordância com a Lei da Proteção de Dados Pessoais de 26 de outubro de 1998 (Lei n.º 67/98). A informação pessoal recolhida pode incluir o seu nome, e-mail, número de telefone e/ou telemóvel, morada, data de nascimento e/ou outros.</p>
-      <p>O uso da ETEC Social pressupõe a aceitação deste Acordo de privacidade. A equipe da ETEC Social reserva-se ao direito de alterar este acordo sem aviso prévio. Deste modo, recomendamos que consulte a nossa política de privacidade com regularidade de forma a estar sempre atualizado.</p>
-      <p>A ETEC Social como uma rede social possui ligações para outros sites, os quais, podem conter informações/ferramentas úteis para os Utilizadores. A nossa política de privacidade não é aplicada a sites de terceiros, pelo que, caso visite outro site a partir do nosso deverá ler a politica de privacidade do mesmo. Não nos responsabilizamos pela política de privacidade ou conteúdo presente nesses mesmos sites.</p>
+      {!! trans('terms.privacidade') !!}
    </div>
 </div>
 </div>
