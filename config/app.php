b69417b126e0e6a80d847bfb36070353e7ec95ac
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-07-04 20:49:56 -0300

    my bad

--- config/app.php
+++ config/app.php
@@ -212,7 +212,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
         // vendor
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
         'Input'     => Illuminate\Support\Facades\Input::class,
