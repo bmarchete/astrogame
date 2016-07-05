cd4952ec9e56c9e39922239cf5e5c3b7bc3230d6
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-07-04 22:27:47 -0300

    corrigido linguagem do navegador

--- app/Http/Middleware/Language.php
+++ app/Http/Middleware/Language.php
@@ -8,32 +8,40 @@ use App;
 class Language
 {
     public $avaliable_langs = ['pt-br' => 'pt-br',
-                               'pt' => 'pt-br', 
-                               'en' => 'en', 
-                               'en-US' => 'en',
-                               'fr' => 'fr', 
-                               'es' => 'es'];
+                               'pt-BR' => 'pt-br',
+                               'pt' => 'pt-br',
+                               'en' => 'en',
+                               'en-US' => 'en'];
     /**
      * Handle an incoming request.
-     * Checa se há uma linguagem settada na sessão, se sim, muda a lang
+     * Checa se há uma linguagem settada na sessão, se sim, muda a lang.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         $language = session()->get('language', $this->browser_lang());
         App::setLocale($language);
+
         return $next($request);
     }
 
-    private function browser_lang(){
-        $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-        if(array_key_exists($lang, $this->avaliable_langs)){
-            return $this->avaliable_langs[$lang];
-        } else {
+    private function browser_lang()
+    {
+        if(!isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])){
             return 'en';
         }
+
+        $languages = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+        foreach ($languages as $lang) {
+            if (array_key_exists($lang, $this->avaliable_langs)) {
+                return $this->avaliable_langs[$lang];
+            }
+        }
+
+        return 'en';
     }
 }
