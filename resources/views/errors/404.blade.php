052c7ee0e22afe542e75b6012eaf26cd923b7566
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-07-06 09:31:56 -0300

    fix error images

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,11 +9,11 @@
             <div class="uk-vertical-align-middle">
                 <div class="uk-width-2-3 uk-container-center uk-text-center">
                     <span class="bubble">
-                      <img src="img/telescope-not.png" width="150" class="uk-animation uk-animation-hover uk-animation-scale">
+                      <img src="{{url('img/telescope-not.png')}}" width="150" class="uk-animation uk-animation-hover uk-animation-scale">
                       <p>Oh céus! Não encontramos nada com esses termos, tente de novo ou volte para a <a href="{{ url('/') }}">página inicial</a></p></span>
                 </div>
                 <div class="uk-width-1-3 uk-container-center">
-                    <img class="galileu-img uk-animation-hover uk-animation-shake" alt="" src="img/char/galileu.png">
+                    <img class="galileu-img uk-animation-hover uk-animation-shake" alt="" src="{{url('img/char/galileu.png')}}">
                 </div>
             </div>
         </div>
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -9,11 +9,11 @@
             <div class="uk-vertical-align-middle">
                 <div class="uk-width-2-3 uk-container-center uk-text-center">
                     <span class="bubble">
-                      <img src="img/telescope-not.png" width="150" class="uk-animation uk-animation-hover uk-animation-scale">
+                      <img src="{{url('img/telescope-not.png')}}" width="150" class="uk-animation uk-animation-hover uk-animation-scale">
                       <p>Estamos em manutenção! Logo voltamos a explorar o universo!</span>
                 </div>
                 <div class="uk-width-1-3 uk-container-center">
-                    <img class="galileu-img uk-animation-hover uk-animation-shake" alt="" src="img/char/galileu.png">
+                    <img class="galileu-img uk-animation-hover uk-animation-shake" alt="" src="{{url('img/char/galileu.png')}}">
                 </div>
             </div>
         </div>
