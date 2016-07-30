a9d2c3fd110810a48de086d53dc907d3b7ea794d
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-07-30 13:43:55 -0300

    routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,23 +18,21 @@ Route::group(['middleware' => 'web'], function () {
 	Route::auth();
 
 	// website-projeto
-	Route::group(['middleware' => 'website'], function(){
-		Route::get('/', 'HomeController@index');
-		Route::get('/home', 'HomeController@home');
-		Route::get('/sobre', 'HomeController@sobre');
-		Route::get('/equipe', 'HomeController@equipe');
-		Route::get('/termos', 'HomeController@termos');
-		Route::get('/politica', 'HomeController@politica');
-		Route::get('/credits', 'HomeController@credits');
+	Route::get('/', 'HomeController@index');
+	Route::get('/home', 'HomeController@home');
+	Route::get('/sobre', 'HomeController@sobre');
+	Route::get('/equipe', 'HomeController@equipe');
+	Route::get('/termos', 'HomeController@termos');
+	Route::get('/politica', 'HomeController@politica');
+	Route::get('/credits', 'HomeController@credits');
 
-		// website-contact
-		Route::get('/contato', 'ContactController@index');
-		Route::post('/contato', 'ContactController@store');
+	// website-contact
+	Route::get('/contato', 'ContactController@index');
+	Route::post('/contato', 'ContactController@store');
 
-		// public profile
-		Route::get('/player/{id}', ['middleware'=> 'game', 'uses' => 'GameController@player'])->where('id', '[0-9]+');
-		Route::get('/ranking', 'HomeController@ranking');
-	});
+	// public profile
+	Route::get('/player/{id}', ['middleware'=> 'game', 'uses' => 'GameController@player'])->where('id', '[0-9]+');
+	Route::get('/ranking', 'HomeController@ranking');
 
 	// website-game
 	Route::group(['middleware' => ['auth', 'game'], 'prefix' => 'game'], function () {
