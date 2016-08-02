33c1ed2b9e5664a04135d468edb004e14c17fa31
Eduardo Augusto Ramos <eduardo.auramos@gmail.com>
2016-08-02 09:19:38 -0300

    Update about.php

--- resources/lang/pt-br/about.php
+++ resources/lang/pt-br/about.php
@@ -25,8 +25,8 @@ return [
 
     'figcaption1' => 'Visita ao planetário de Campinas com a professora Marli',
 
-    'text10' => 'Tudo começou com uma ideia que surgiu numa conversa entra os idealizadores Brenda e Eduardo: - Sabe o que seria legal? Criar um simluador do universo.',
-    'text11' => 'A partir dai a ideia não parou mais, cada vez foram surgindo outras maluquices, mas de nossos amigos curtiram e começaram a apoiar também. Arregaçamos a manga, visitamos o planetário municipal de Campinas, localizado no Taquaral. Pesquisamos muito, organizamos uma equipe de mais de 40 pessoas, e realizamos um dos melhores projetos da ETEC Pedro Ferreira Alves durante a Expoete 2015, havia até fila para entrar no stand, e muitos ficaram sem conhecer.',
+    'text10' => 'O projeto foi idealizado a partir da discussão entre dois dos integrantes da equipe, Eduardo e Brenda, que decidiram que abordar o tema universo era uma ótima possibilidade para criar um projeto bem recebido pelo público.',
+    'text11' => 'Diante dessa decisão, o projeto começou a ser desenvolvido, e mais pessoas foram adicionadas no mesmo, cerca de 40 pessoas, ajudando na montagem, apresentação e criação. Várias pesquisas foram realizadas previamente para que houvesse uma base informacional por trás do projeto; O planetário municipal de Campinas, por exemplo, nos recebeu e nos ofereceu diversas informações interessantes para o projeto; o simulador serviu de grande inspiração para a construção para o nosso próprio. O projeto final - Projeto Cosmos, foi apresentado ao público durante a Expoete 2015 da ETEC Pedro Ferreira Alves, recebendo diversas visitaçações e, devido a popularidade do projeto, aconteceram filas para entrar no stand; infelizmente muitos ficaram sem conhecer.',
     'text12' => 'Com base no ano internacional da luz, foi escolhido o tema Luz, Ciência e Vida para a XIV Expoete da ETEC Pedro Ferreira Alves. Pensando nisso foi elaborado nosso projeto, que procura abordar sobre a LUZ que é emitida no universo e observada por nós.',
     'text13' => 'O projeto tem como objetivo principal despertar o interesse do público em nosso stand durante os dias de exposição. Além de divulgar conhecimentos básicos sobre astronomia com base em livros e conteúdos diversos disponíveis atualmente.',
     'text14' => 'Durante o desenvolvimento do projeto tentamos abordar uma linguagem mais voltada para o público adolescente, pois nosso maior público alvo se encontra nessa faixa etária, porém todas as outras faixas etárias também podem usufruir do projeto.',
@@ -41,4 +41,11 @@ return [
     'text17' => 'O percurso começa com pequenos stands explicando um pouco sobre cada conteúdo do COSMOS e termina num simulador, onde tudo o que foi exposto nas apresentações do percurso será reapresentado de uma forma resumida e visual, trazendo a sensação de submersão espacial dentro de um ambiente fechado e escuro. Neste simulador teremos a presença de uma pessoa que fará o papel de capitão da nave; pessoa que será responsável pelos visitantes e tudo o mais que ocorrer na sala. Não recomendado para claustrofóbicos, pessoas com medo do escuro, problemas cardíacos.',
 
     'title4' => 'Membros do Projeto Cosmos',
+    
+    'depoimento1' => 'Foi um projeto muito diferente, nunca havia visto nada igual. Adorei!',
+    'depoimento2' => 'Não sei dizer o que mais gostei no projeto, mas amei o simulador, foi surreal.',
+    'depoimento3' => 'Ótimo projeto, com muitas pessoas dedicadas. Me fez gostar ainda mais do universo!',
+    'depoimento4' => 'Foi uma oportunidade incrível trabalhar com essas pessoas, realizamos algo e estamos orgulhosos do mesmo.',
+    'depoimento5' => 'Foi incrível, o ambiente estava totalmente submersivo. Estão de parabéns!',
+    
 ];
