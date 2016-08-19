1d461d0a622eee1a7c67b82a91488a5a3988676a
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-08-19 20:27:58 -0300

    fix remove avatar div

--- resources/views/game/general/modals.blade.php
+++ resources/views/game/general/modals.blade.php
@@ -155,7 +155,9 @@
                       <input type="file" name="avatar" accept="image/*" id="avatar-file" data-uk-tooltip title="Alterar avatar">
                   </div>
                 </figure>
-                <a id="remove-avatar"><i class="uk-icon-trash"></i> Remover avatar</a>
+                <div>
+                  <a id="remove-avatar"><i class="uk-icon-trash"></i> Remover avatar</a>
+                </div>
             </div>
             <div class="uk-width-medium-5-6">
                 <ul class="uk-list">
