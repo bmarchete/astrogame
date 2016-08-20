49edac4c8170550c101c55af2e53be688e4fc2ca
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-08-20 11:08:12 -0300

    missing js

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    var apollo_11 = new buzz.sound('sounds/effects/apollo_11_speech.mp3', {preload: true, loop: false});
+document.addEventListener("DOMContentLoaded", function(event) {
+    var apollo_11 = new buzz.sound('sounds/effects/apollo_11_speech.mp3', {
+        preload: true,
+        loop: false
+    });
     apollo_11.setVolume(50);
-    $(".astronaut").click(function(){
+    $(".astronaut").click(function() {
         apollo_11.togglePlay();
+    });
+
+
+    $(window).scroll(function() {
+        var yPos = -($(window).scrollTop() / 5) + 550;
+        var bgpos = 'right ' + yPos + 'px';
+
+        $('.contact').css('background-position', bgpos);
     });
 });
