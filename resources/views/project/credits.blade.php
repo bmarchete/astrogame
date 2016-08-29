5bd7ca94ad1b0903dfeab34dd7b20e106ee8aa57
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-08-29 19:39:48 -0300

    Update créditos

--- resources/views/project/credits.blade.php
+++ resources/views/project/credits.blade.php
@@ -2,7 +2,9 @@
 @section('content')
 <div class="thumbnav">
     <div class="uk-container uk-container-center">
+        <div class="uk-align-left">
         <h1>Créditos</h1>
+      </div>
     </div>
 </div>
 <div class="white">
@@ -12,25 +14,40 @@
             <h1>Referencias bibliográficas</h1>
 
             <ul class="uk-list-space">
-            	<b>Passar todas as referencias aqui para o padrão!</b>
-                <li>Costa, J. R. V. Galileu Galilei. Astronomia no Zênite, jan 2006. Disponível em: <a href="http://www.zenite.nu/galileu-galilei/">http://www.zenite.nu/galileu-galilei/</a></li>
-                <li><a href="http://www.sobiologia.com.br/conteudos/Universo/estrelas.php">http://www.sobiologia.com.br/conteudos/Universo/estrelas.php</a></li>
-                <li><a href="http://astro.if.ufrgs.br/const.htm">http://astro.if.ufrgs.br/const.htm</a></li>
-                <li><a href="http://historiadafisicauc.blogspot.com.br/2011/06/galileu-galilei-e-astronomia.html">http://historiadafisicauc.blogspot.com.br/2011/06/galileu-galilei-e-astronomia.html</a></li>
-                <li><a href="http://www.sobiologia.com.br/conteudos/Universo/sistemasolar.php">http://www.sobiologia.com.br/conteudos/Universo/sistemasolar.php</a></li>
-                <li><a href="http://www.suapesquisa.com/sistemasolar/">http://www.suapesquisa.com/sistemasolar/</a></li>
-                <li><a href="http://www.infoescola.com/biografias/nicolau-copernico/">http://www.infoescola.com/biografias/nicolau-copernico/</a></li>
-                <li><a href="http://www.sofisica.com.br/conteudos/Mecanica/GravitacaoUniversal/lk.php">http://www.sofisica.com.br/conteudos/Mecanica/GravitacaoUniversal/lk.php</a></li>
-            </ul>
-            <hr class="uk-grid-divider">
-            <h1>Créditos</h1>
-            <ul class="uk-list-space">
-                <li>http://www.eso.org/public/</li>
+              <li>Balduino, P. Dominando JavaScript com jQuery. Casa do Código, 2012.</li>
+              <li>Biografias - Galileu Galilei - Só História. Disponível em: <a href="http://www.sohistoria.com.br/biografias/galileu/">http://www.sohistoria.com.br/biografias/galileu/</a>. Acesso em: 17 ago. 2016.</li>
+              <li>buzz-website. Disponível em: <a href="http://buzz.jaysalvat.com/documentation/buzz/">http://buzz.jaysalvat.com/documentation/buzz/</a>. Acesso em: 10 ago. 2016.</li>
+              <li>Constelações. Disponível em: <a href="http://astro.if.ufrgs.br/const.htm">http://astro.if.ufrgs.br/const.htm</a>. Acesso em: 3 mai. 2016.</li>
+              <li>Digital, C. Biografia de Galileu Galilei. Disponível em: <a href="http://www.suapesquisa.com/biografias/galileu/">http://www.suapesquisa.com/biografias/galileu/</a>. Acesso em: 17 ago. 2016.</li>
+              <li>Digital, C. Nicolau Copérnico - vida, estudos de astronomia, heliocentrismo, biografia. Disponível em: <a href="http://www.suapesquisa.com/pesquisa/copernico.htm">http://www.suapesquisa.com/pesquisa/copernico.htm</a>. Acesso em: 17 ago. 2016.</li>
+              <li>Estrelas - Só Biologia: Disponível em: <a href="http://www.sobiologia.com.br/conteudos/Universo/estrelas.php">http://www.sobiologia.com.br/conteudos/Universo/estrelas.php</a>. Acesso em: 5 mar. 2016.</li>
+              <li>Galileu Galilei - Biografias - UOL Educação. Disponível em: <a href="http://educacao.uol.com.br/biografias/galileu-galilei.htm">http://educacao.uol.com.br/biografias/galileu-galilei.htm</a>. Acesso em: 17 ago. 2016.</li>
+              <li>Galileu Galilei - Cola da Web. Disponível em: <a href="http://www.coladaweb.com/fisica/fisica-geral/galileu-galilei">http://www.coladaweb.com/fisica/fisica-geral/galileu-galilei</a>. Acesso em: 17 ago. 2016.</li>
+              <li>Galileu Galilei. Disponível em: <a href="http://www.infoescola.com/biografias/galileu-galilei/">http://www.infoescola.com/biografias/galileu-galilei/</a>. Acesso em: 17 ago. 2016.</li>
+              <li>Gama, p. Introdução: JSON. Disponível em: <a href="http://www.devmedia.com.br/introducao-json/23166">http://www.devmedia.com.br/introducao-json/23166</a>. Acesso em: 4 jul. 2016</li>
+              <li>Git - Documentation. Disponível em: <a href="https://git-scm.com/documentation">https://git-scm.com/documentation</a>. Acesso em: 6 jun. 2016.</li>
+              <li>HTML5. Disponível em: <a href="https://developer.mozilla.org/pt-BR/docs/Web/HTML/HTML5">https://developer.mozilla.org/pt-BR/docs/Web/HTML/HTML5</a>. Acesso em: 9 ago. 2016.</li>
+              <li>Johannes Kepler - Biografia. Disponível em: <a href="http://www.infoescola.com/astronomia/johannes-kepler/">http://www.infoescola.com/astronomia/johannes-kepler/</a>. Acesso em: 17 ago. 2016.</li>
+              <li>Leis de Kepler - NASA Observatorium Education. Disponível em: <a href="http://astro.if.ufrgs.br/Orbit/orbits.htm">http://astro.if.ufrgs.br/Orbit/orbits.htm</a>. Acesso em: 17 ago. 2016.</li>
+              <li>Luis Fardo, M. A GAMIFICAÇÃO APLICADA EM AMBIENTES DE APRENDIZAGEM. Traducao . 1. ed. Caxias do Sul. Disponível em: <a href="http://seer.ufrgs.br/renote/article/viewFile/41629/26409">http://seer.ufrgs.br/renote/article/viewFile/41629/26409</a>. Acesso em 17 ago. 2016.</li>
+              <li>Matéria, T.Kepler, L. Leis de Kepler. Disponível em: <a href="https://www.todamateria.com.br/leis-de-kepler/">https://www.todamateria.com.br/leis-de-kepler/</a>. Acesso em: 17 ago. 2016.</li>
+              <li>Meira, A. Blog do Adriano Meira Usando a função Ajax ($.ajax) do jQuery. Disponível em: <a href="http://www.adrianomeira.com/blog/usando-a-funcao-ajax-do-jquery">http://www.adrianomeira.com/blog/usando-a-funcao-ajax-do-jquery</a>. Acesso em: 3 jul. 2016.</li>
+              <li>MySQL: MySQL Documentation. Disponível em: <a href="https://dev.mysql.com/doc/">https://dev.mysql.com/doc/</a>. Acesso em: 5 jul. 2016.</li>
+              <li>Nicolau Copérnico - Biografia. Disponível em: <a href="http://www.infoescola.com/biografias/nicolau-copernico/">http://www.infoescola.com/biografias/nicolau-copernico/</a>. Acesso em: 17 ago. 2016.</li>
+              <li>Nicolau Copérnico - Biografias - UOL Educação. Disponível em: <a href="http://educacao.uol.com.br/biografias/nicolau-copernico.htm">http://educacao.uol.com.br/biografias/nicolau-copernico.htm</a>. Acesso em: 17 ago. 2016.</li>
+              <li>Nicolau Copérnico. Disponível em: <a href="http://www.e-biografias.net/nicolau_copernico/">http://www.e-biografias.net/nicolau_copernico/</a>. Acesso em: 17 ago. 2016.</li>
+              <li>Niederauer, J. Desenvolvendo Websites com PHP. Tradução. 1. ed. [s.l.] Novatec, 2016. Disponível em: <a href="http://novatec.com.br/livros/php2ed/capitulo9788575222348.pdf">http://novatec.com.br/livros/php2ed/capitulo9788575222348.pdf</a>. Acesso em: 15 jul. 2016.</li>
+              <li>Normas para elaboração de trabalhos acadêmicos. Curitiba: UTFPR, 2009. Disponível em: <a href="http://www.utfpr.edu.br/documentos/normas_trabalhos_utfpr.pdf">http://www.utfpr.edu.br/documentos/normas_trabalhos_utfpr.pdf</a>. Acesso em: 3 jun. 2016.</li>
+              <li>Official Construct 2 Manual - Construct 2 Manual. Disponível em: <a href="https://www.scirra.com/manual/1/construct-2">https://www.scirra.com/manual/1/construct-2</a>. Acesso em: 2 ago. 2016.</li>
+              <li>Otwell, T.Carmo, V. Instalação - Laravel - O Framework PHP para os Artesãos da Web. Disponível em: <a href="http://laravel.artesaos.org/docs/master">http://laravel.artesaos.org/docs/master</a>. Acesso em: 2 jul. 2016.</li>
+              <li>Só Física - Johannes Kepler. Disponível em: <a href="http://www.sofisica.com.br/conteudos/Biografias/Johannes_Kepler.php">http://www.sofisica.com.br/conteudos/Biografias/Johannes_Kepler.php</a>. Acesso em: 17 ago. 2016.</li>
+              <li>Só Física - Leis de Kepler. Disponível em: <a href="http://www.sofisica.com.br/conteudos/Mecanica/GravitacaoUniversal/lk.php">http://www.sofisica.com.br/conteudos/Mecanica/GravitacaoUniversal/lk.php</a>. Acesso em: 17 ago. 2016.</li>
+              <li>UIkit documentation. Disponível em: <a href="http://getuikit.com/docs/documentation_get-started.html">http://getuikit.com/docs/documentation_get-started.html</a>. Acesso em: 4 jul. 2016.</li>
             </ul>
             <hr class="uk-grid-divider">
             <h1>Agredecimentos</h1>
             <ul class="uk-list-space">
-                <li>Professor Jefferson David ... pela revisão geral do trabalho</li>
+                <!-- <li>Professor Jefferson David ... pela revisão geral do trabalho</li> -->
             </ul>
     		<hr class="uk-grid-divider">
         </div>
