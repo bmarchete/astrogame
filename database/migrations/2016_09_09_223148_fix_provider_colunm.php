6598692055f2288ab8c8e48b21e29fac43787a72
Eduardo Ramos <eduardo.auramos@gmail.com>
2016-09-09 22:36:58 -0300

    Provider id migration fix

---
+++ database/migrations/2016_09_09_223148_fix_provider_colunm.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixProviderColunm extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+          $table->string('provider_user_id', 255)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
