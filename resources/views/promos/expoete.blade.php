e62b7824e5089469106ab965e4c8e4f1c4651f58
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2016-10-07 07:51:55 -0300

    ultimo layout expoete :)

--- resources/views/promos/expoete.blade.php
+++ resources/views/promos/expoete.blade.php
@@ -7,9 +7,9 @@
 
     <title>Expoete 2016</title>
 
-    {!! Minify::stylesheet(['/vendor/uikit/css/normalize.css', '/vendor/uikit/css/uikit.gradient.css', '/css/project/expoete.css'])->withFullUrl() !!} @yield('style')
+    {!! Minify::stylesheet(['/vendor/uikit/css/normalize.css', '/vendor/uikit/css/uikit.gradient.css', '/css/project/expoete.css'])->withFullUrl() !!}
     <link rel="icon" type="image/png" href="/img/favicon/favicon-32x32.png" sizes="32x32">
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" media="none" onload="if(media!='all')media='all'">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     <style>
         body {
             overflow: hidden;
@@ -19,12 +19,11 @@
 
 <body>
     <div class="uk-container uk-container-center">
-        <video src="http://localhost/trailer.mp4" width="1080" autoplay style="position: fixed; right: 10%;padding: 6px;background: #fff;border-radius: 3px;margin: 10px" loop></video>
-
-        <a href="http://avellan.com.br">
-            <img src="{{ asset('img/avellan.png')}}" width="250" style="position: fixed;bottom: 0;right: 0;">
-        </a>
+        <video src="http://localhost/trailer_sem_logo.mp4" width="1080" autoplay style="position: fixed; right: 10%;padding: 6px;background: #fff;border-radius: 3px;margin: 10px" loop></video>
     </div>
-    <img src="{{ asset('img/expoete_qrcode.jpg')}}" width="130" style="position: fixed; bottom: 1%; left: 1%">
+    <img src="{{ asset('img/expoete_qrcode.jpg')}}" width="250" style="position: fixed; bottom: 1%; left: 1%">
+    <a href="https://astrogame.me" style="font-size: 72px; color: #fff; position: fixed; bottom: 10%; left: 30%; font-weight: bold">www.astrogame.me</a>
+
+    <img src="{{ asset('img/avellan.png')}}" alt="" style="position: fixed; right: 0; bottom: 2%; width: 350px">
 </body>
 </html>
